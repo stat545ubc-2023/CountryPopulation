50da07edb0a9787a4ab14db0a4485926b96bb561
Berke Ucar <berke.ucar@ug.bilkent.edu.tr>
2023-11-22 20:28:28 -0800

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# Country Population
 
+This is the assignment 3 of STAT545B and developed by Berke Ucar. 
+
+*CountryPopulation* is a `shinyr` app that shows the population of countries over the period 1800 to 2100. 1800-2015 band of this period is the historical population counts, while 2015-2100 period is projected. The way of representation of this data is a histogram and a data table. This application has the following major properties:
+
+1. Users can select multiple countries to represent the populations on the graph and the table.
+2. Users can transform the population counts into the logarithmic scale.
+3. Users can select to include the projections or not.
+4. Users can interact with data table. The table is presented according to the country preference as well. 
+  * Pagination
+  * Sorting based on different columns
+  * Searching on the table
+
+The link to the application: *https://berkeucar.shinyapps.io/CountryPopulation/*
+
+This application uses the dataset contains the countries' historical and projected populations subsampled from Gapminder Version 5. Gapminder uses UN Population Revision. Further details can be accessed from the [link](https://github.com/owid/owid-datasets/tree/master/datasets/Population%20by%20country%2C%201800%20to%202100%20(Gapminder%20%26%20UN)).
+
+### Dataset reference:
+Mathieu E., et al. “Population by Country, 1800 to 2100 (Gapminder & UN).” _GitHub_, github.com/owid/owid-datasets/tree/master/datasets/Population%20by%20country%2C%201800%20to%202100%20(Gapminder%20%26%20UN). Accessed 22 Nov. 2023. 
--- app.R
+++ app.R
@@ -1,3 +1,7 @@
+# @author: Berke Ucar
+# @version: 0.1.0
+# @date: Nov 22, 2023
+
 library(shiny)
 library(here)
 library(tidyverse)
@@ -6,60 +10,62 @@ library(shiny.fluent)
 
 # read the csv file that contains the dataset into a tibble
 dataset <- as_tibble(read.csv(here::here("population.csv")))
+# change the column names in a way that it is more meaningful/traceable
 dataset <- rename(dataset, country = Entity,
                   year=Year,
                   historical_population = "Population.by.country..1800.to.2015..Gapminder...UN.", 
                   projected_population = "Population.by.country..historic.and.projections..Gapminder...UN.")
-    
-ui <- fluidPage(
-    theme = bs_theme(version = 4, bootswatch = "sandstone"),
-    # Application title
-    
-    headerPanel("Country Population by Years"),
 
-    # Sidebar with a slider input for number of bins 
+# User Interface component of the application
+ui <- fluidPage(
+    theme = bs_theme(version = 4, bootswatch = "sandstone"), # Feature no 1: Adding a Theme - I don't know about you but I believe themes are necessary for applications to make the UI appealing to the user.
+    headerPanel("Country Population Over the Years"), # the title of the page
+
     sidebarLayout(
         sidebarPanel(
-          style = "max-height:90vh; height: 90vh; overflow-y: auto;", 
-          Toggle.shinyInput("logToggle", FALSE, label="Log10 Transformation for populations"),
-          Toggle.shinyInput("projectionToggle", FALSE, label="Include projections after 2015"),
+          style = "max-height:91vh; height: 91vh; overflow-y: auto;", # Feature no 2: Scrollbar to the sidebar - This allows the sidebar to have a scrollbar, which was necessary to keep the operator focused on the graphs rather then scrolling the graph with the checkboxes as well.
+          Toggle.shinyInput("logToggle", FALSE, label="Log10 Transformation for populations"), # Feature no 3: Toggle input for logarithmic transformation on y axis - This is important since sometimes there is a huge difference between populations of different countries and user cannot see the low-populated country in the graph. This allows to bring those numbers closer and make the populations visible for all the countries.
+          Toggle.shinyInput("projectionToggle", FALSE, label="Include projections after 2015"), # Feature no 4: Toggle input for inclusion of the projected population for countries - This dataset contains the predicted populations for the countries after 2015, which may not be irrelevant for all the users. So, there is a toggle bar to include the projections.
           checkboxGroupInput( inputId="countryCheckbox", label="Choose country (or countries) to show in graph:",
                               choices=sort(unique(dataset$country)), 
                               selected="Afghanistan"
             
-          )
+          ) # Feature no 5: Checkbox inputs for the countries - This option allows users to select different countries to represent on the graph and table. This is relevant for users to compare population of multiple countries on the graph and/or the table.
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-          style = "max-height:90vh; height: 90vh; overflow-y: auto;", 
+          style = "max-height:91vh; height: 91vh; overflow-y: auto;", # Feature no 6: Scrollbar to the main panel - This allows users to scroll up or down in order to see the table or graph clearer while not changing the focus on the options and see what they selected without scrolling up and down with the main panel.
           plotOutput("plot"),
-          DT::dataTableOutput("table")
+          DT::dataTableOutput("table") # Feature no 7: Paginated Data Table - This allows users to search, paginate, sort the datatable in a neat manner. This is relevant since some users may need to check specific years or populations. Also, it is convenient and appealing since it is paginated as well.
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Server part of the application
 server <- function(input, output) {
+  # chooses a subset of the data table using the checkbox and projection toggle input
   subset <- reactive({
-    if (is.null(input$countryCheckbox)) {
-      return(NULL)
+    if (is.null(input$countryCheckbox) | is.null(input$projectionToggle)) {
+      return()
     }    
-    if (input$projectionToggle)
-      dataset %>% filter(country==input$countryCheckbox) %>% group_by(country)
+    if (input$projectionToggle){
+      dataset %>% filter(country%in%input$countryCheckbox) %>% group_by(country)
+    }
     else
-      dataset %>% filter(country==input$countryCheckbox & year <= 2015) %>% group_by(country)
+      dataset %>% filter(country%in%input$countryCheckbox & year <= 2015) %>% group_by(country)
   })
   
+  # Generates the graph based on the logartihmic transformation toggle and data set subsampled with subset method
   output$plot <- renderPlot({
     if (is.null(subset()))
-      return()
+      return(NULL)
     
     if (input$logToggle){
       ggplot(subset()) + 
         geom_bar(aes(year, projected_population, fill=country),stat = "identity") +
         scale_y_log10("Population (Scale: log10)") +
-        xlab("Year")
+        xlab("Year") 
     }
     else{
       ggplot(subset()) + 
@@ -70,6 +76,7 @@ server <- function(input, output) {
     
   })
   
+  # Generates the table based on the result generated with subset method
   output$table <- DT::renderDataTable({
     subset()
   })
